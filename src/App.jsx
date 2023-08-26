9d0d76ba8fd9fb44bc5c7309e085ecee558aedf6
naszifnaaz <naszifnaaz@gmail.com>
2023-08-27 02:50:08 +0530

    ci: footer-component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Navbar } from "./components/common/Navbar";
 import { TechStack } from "./components/techstack/TechStack";
 import { Home } from "./components/home/Home";
 import { Projects } from "./components/projects/Projects";
+import { Footer } from "./components/common/Footer";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Home />
       <TechStack />
       <Projects />
+      <Footer />
     </>
   );
 }
